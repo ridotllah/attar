51f04fc8a7a90e4266ecd737b47420de5263147d
ridotllah <akhtarsyah10@gmail.com>
2025-04-28 19:03:43 +0800

    Tugas PHP Attar!

---
+++ tugas.php
@@ -0,0 +1,4 @@
+<?php
+echo "Nama :Muh. Akhtarsyah"
+echo "Tempat Tanggal Lahir :Makassar 1, November, 2008"
+echo "Alamat :Sudiang Jl. Arung Teko No.99, Sudiang, Kec. Biringkanaya, Kota Makassar, Sulawesi Selatan"
