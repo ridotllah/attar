32db0fec3d03188ae9b0c2bc5a68d2501bbfdd57
ridotllah <akhtarsyah10@gmail.com>
2025-04-28 19:17:53 +0800

    Tugas Quiz PHP Attar

---
+++ tugas5.php
@@ -0,0 +1,9 @@
+<?php
+$nilai_matematika = 65;
+$nilaibahasa = 55;
+
+if ($nilai_matematika >= 80 && $nilai_bahasa >= 70) {
+echo "selamat! kamu boleh pergi jalan";}
+else {echo "YAAh! belajar lagi masss";
+}
+?>
