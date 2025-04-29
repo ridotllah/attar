1ae34265d85e0e32f46e32812ccb265f8ff70787
ridotllah <akhtarsyah10@gmail.com>
2025-04-29 14:30:47 +0800

    tgasquiz6 php

---
+++ tugas6.php
@@ -0,0 +1,12 @@
+<?php
+$nilaikun = 94;
+
+if ($nilaikun>= 90) {
+    echo "Sangat mampu";
+} elseif ($nilaikun>= 80) {
+    echo "Baik";
+} elseif ($nilaikun>= 75) {
+    echo "Cukup";
+} else {
+    echo "Tidak mampu";
+}
